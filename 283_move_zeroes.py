716127922fe0d71d3b525f3cab8eca2e40828b75
Anson Wan <anson.hy.wan@gmail.com>
2024-06-28 15:44:48 -0400

    remove logic check redundancy

--- 283_move_zeroes.py
+++ 283_move_zeroes.py
@@ -6,7 +6,7 @@ class Solution:
         # To do in place (no creating new array), we need to use two-pointer technique
         l = 0
         for r in range(1, len(nums)):
-            if nums[l] != 0 and nums[r] != 0:
+            if nums[l] != 0:
                 l += 1
             if nums[l] == 0 and nums[r] != 0:
                 temp = nums[l]
